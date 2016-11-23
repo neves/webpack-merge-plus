0e449d9812edb55cc6916417d48d0293cc387e08
Marcos Neves <marcos.neves@gmail.com>
2016-11-23 10:40:59 -0200

    all tests green

--- .test.js
+++ .test.js
@@ -7,3 +7,94 @@ test('behave like webpack-merge', () => {
 
   expect(merge(a, b)).toEqual(result)
 })
+
+test('run functions', () => {
+  a = () => {
+    return {foo: 'foo'}
+  }
+  b = () => {
+    return {bar: 'bar'}
+  }
+  result = {foo: 'foo', bar: 'bar'}
+
+  expect(merge(a, b)).toEqual(result)
+})
+
+test('pass merged into function', () => {
+  a = {foo: 'foo'}
+
+  b = (a) => {
+    return {bar: a.foo}
+  }
+  result = {foo: 'foo', bar: 'foo'}
+
+  expect(merge(a, b)).toEqual(result)
+})
+
+test('merge nested array', () => {
+  a = [
+    [
+      {foo: 'foo'},
+      {a: 'a'}
+    ],
+    {bar: 'bar'}
+  ]
+
+  b = [
+    {jon: 'jon'},
+    [
+      {doo: 'doo'},
+      {b: 'b'}
+    ]
+  ]
+  result = {foo: 'foo', a: 'a', bar: 'bar', jon: 'jon', doo: 'doo', b: 'b'}
+
+  expect(merge(a, b)).toEqual(result)
+})
+
+test('merge nested array with function', () => {
+  a = [
+    [
+      {foo: 'foo'},
+      {a: 'a'},
+      merged => { return {f: merged} }
+    ],
+    {bar: 'bar'}
+  ]
+
+  b = [
+    {jon: 'jon'},
+    [
+      {doo: 'doo'},
+      {b: 'b'}
+    ]
+  ]
+  result = {foo: 'foo', a: 'a', f: {foo: 'foo', a: 'a'}, bar: 'bar', jon: 'jon', doo: 'doo', b: 'b'}
+
+  expect(merge(a, b)).toEqual(result)
+})
+
+test('function that returns array', () => {
+  a = () => {
+    return [
+      [
+        {foo: 'foo'},
+        {a: 'a'},
+        merged => { return {f: merged} }
+      ],
+      {bar: 'bar'}
+    ]
+  }
+
+  b = [
+    {jon: 'jon'},
+    [
+      {doo: 'doo'},
+      {b: 'b'}
+    ]
+  ]
+
+  result = {foo: 'foo', a: 'a', f: {foo: 'foo', a: 'a'}, bar: 'bar', jon: 'jon', doo: 'doo', b: 'b'}
+
+  expect(merge(a, b)).toEqual(result)
+})
--- index.js
+++ index.js
@@ -1,5 +1,36 @@
 const merge = require('webpack-merge').smart
 
 module.exports = function () {
-  return merge.apply(null, arguments)
+  return mergeArray(toArray(arguments))
+}
+
+function mergeArray (items) {
+  return items.reduce((a, b) => mergePlus(a, b))
+}
+
+function toArray (arrayLike) {
+  return [].concat.apply([], arrayLike)
+}
+
+function mergePlus (a, b) {
+  a = convert(a, {})
+  b = convert(b, a)
+
+  return merge(a, b)
+}
+
+function convert (item, before) {
+  if (isFunction(item)) {
+    item = item(before)
+  }
+
+  if (Array.isArray(item)) {
+    item = mergeArray(item)
+  }
+
+  return item
+}
+
+function isFunction (f) {
+  return f instanceof Function
 }
