9c9e1f99cadfcfe680281055ca3b972581b3cef3
Marcos Neves <marcos.neves@gmail.com>
2016-11-24 10:32:31 -0200

    Add eslint

--- example/webpack.config.js
+++ example/webpack.config.js
@@ -1,11 +1,11 @@
-const mergePlus = require ('../index.js')
+const mergePlus = require('../index.js')
 
 const conf = mergePlus([
   {context: __dirname},
   require('./confs/base'),
   require('./confs/sourceMap'),
   require('./confs/server'),
-  require('./confs/publicPath'),
+  require('./confs/publicPath')
 ])
 
 if (process.env.DEBUG) {
